d96cbf92b0f764af2a74f34cdcb0d446977bb7c5
xuwei-k <6b656e6a69@gmail.com>
2022-03-16 07:27:12 +0900

    SuppressWarnings args

--- core/src/main/scala-3/org/wartremover/WartUniverse.scala
+++ core/src/main/scala-3/org/wartremover/WartUniverse.scala
@@ -2,22 +2,29 @@ package org.wartremover
 
 import scala.quoted.Quotes
 import scala.quoted.Type
+import java.lang.SuppressWarnings
 
 class WartUniverse(val quotes: Quotes, traverser: WartTraverser, onlyWarning: Boolean) { self =>
   abstract class Traverser extends quotes.reflect.TreeTraverser {
-    private[this] def name = traverser.getClass.getSimpleName.dropRight(1)
+    private[this] def simpleName = traverser.getClass.getSimpleName.dropRight(1)
+    private[this] def fullName = traverser.getClass.getName.dropRight(1)
 
-    protected def messagePrefix = s"[wartremover:${name}] "
+    protected def messagePrefix = s"[wartremover:${simpleName}] "
     final implicit val q: self.quotes.type = self.quotes
     import q.reflect.*
     def hasWartAnnotation(t: Tree): Boolean = {
-      t.symbol.getAnnotation(TypeTree.of[java.lang.SuppressWarnings].symbol) match {
-        case Some(a) =>
-          a // TODO
-          true
-        case _ =>
-          false
-      }
+      val args: Set[String] = t.symbol.getAnnotation(TypeTree.of[SuppressWarnings].symbol).collect {
+        case a1 if a1.isExpr =>
+          PartialFunction.condOpt(a1.asExpr) {
+            case '{ new SuppressWarnings($a2: Array[String]) } =>
+              PartialFunction.condOpt(a2.asTerm) {
+                case Apply(Apply(_, Typed(e, _) :: Nil), _) =>
+                  e.asExprOf[Seq[String]].value
+              }.flatten
+          }.flatten
+      }.flatten.toList.flatten.toSet
+
+      args.contains(fullName) || args("org.wartremover.warts.All")
     }
 
     protected final def warning(u: WartUniverse)(pos: Position, message: String): Unit = {
