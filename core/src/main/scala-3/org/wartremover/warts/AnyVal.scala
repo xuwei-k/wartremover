3fa1fe37ed79974186404b2e835986a12a8977d7
xuwei-k <6b656e6a69@gmail.com>
2022-03-15 14:37:47 +0900

    a

--- core/src/main/scala-3/org/wartremover/warts/AnyVal.scala
+++ core/src/main/scala-3/org/wartremover/warts/AnyVal.scala
@@ -1,11 +1,9 @@
 package org.wartremover
 package warts
 
-import dotty.tools.dotc.ast.tpd.InferredTypeTree
-
 object AnyVal extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     implicit val q = u.quotes
-    new u.ForbidInferenceTraverser[AnyVal]
+    new u.ForbidInferenceTraverser[scala.AnyVal]
   }
 }
