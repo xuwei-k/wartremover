8e8a20e2e9b542a855e1462f9a846b1e86e25025
xuwei-k <6b656e6a69@gmail.com>
2022-03-16 18:13:58 +0900

    TraversableOps

--- core/src/main/scala-3/org/wartremover/warts/TraversableOps.scala
+++ core/src/main/scala-3/org/wartremover/warts/TraversableOps.scala
@@ -6,12 +6,43 @@ object TraversableOps extends WartTraverser {
     new u.Traverser {
       import q.reflect.*
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+        def err(method: String, alternative: String) =
+          error(u)(tree.pos, s"${method} is disabled - use ${alternative} instead")
+
         tree match {
           case t if hasWartAnnotation(t) =>
           case t if t.isExpr =>
             t.asExpr match {
-              case '{ $x } =>
-                x
+              case '{ ($x: collection.Iterable[t]).head } =>
+                err("head", "headOption")
+              case '{ ($x: collection.Iterable[t]).tail } =>
+                err("tail", "drop(1)")
+              case '{ ($x: collection.Iterable[t]).init } =>
+                err("init", "dropRight(1)")
+              case '{ ($x: collection.Iterable[t]).last } =>
+                err("last", "lastOption")
+              case '{ ($x: collection.Iterable[t]).reduce($f) } =>
+                err("reduce", "reduceOption or fold")
+              case '{ ($x: collection.Iterable[t]).reduceLeft($f) } =>
+                err("reduceLeft", "reduceLeftOption or foldLeft")
+              case '{ ($x: collection.Iterable[t]).reduceRight($f) } =>
+                err("reduceRight", "reduceRightOption or foldRight")
+              case '{
+                      type t1
+                      type t2
+                      ($x: collection.Iterable[`t1`]).maxBy($f: Function[`t1`, `t2`])($o: Ordering[`t2`])
+                    } =>
+                err("maxBy", "foldLeft or foldRight")
+              case '{ ($x: collection.Iterable[t]).max($o) } =>
+                err("max", "foldLeft or foldRight")
+              case '{
+                      type t1
+                      type t2
+                      ($x: collection.Iterable[`t1`]).minBy($f: Function[`t1`, `t2`])($o: Ordering[`t2`])
+                    } =>
+                err("minBy", "foldLeft or foldRight")
+              case '{ ($x: collection.Iterable[t]).min($o) } =>
+                err("min", "foldLeft or foldRight")
               case _ =>
                 super.traverseTree(tree)(owner)
             }
