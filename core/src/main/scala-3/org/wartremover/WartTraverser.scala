f9e017ef5d3b955ae6ff0f8e7b858a3d5318d45b
xuwei-k <6b656e6a69@gmail.com>
2022-03-16 15:46:20 +0900

    wip

--- core/src/main/scala-3/org/wartremover/WartTraverser.scala
+++ core/src/main/scala-3/org/wartremover/WartTraverser.scala
@@ -1,5 +1,22 @@
 package org.wartremover
 
-abstract class WartTraverser{
+abstract class WartTraverser{ self =>
   def apply(u: WartUniverse): u.Traverser
+
+  def compose(o: WartTraverser): WartTraverser = new WartTraverser {
+    override def apply(u: WartUniverse): u.Traverser = {
+      import u.quotes.reflect.*
+      new u.Traverser {
+        override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+          self.apply(u).traverseTree(tree)(owner)
+          o.apply(u).traverseTree(tree)(owner)
+        }
+      }
+    }
+  }
+}
+
+object WartTraverser {
+  def sumList(u: WartUniverse)(l: List[WartTraverser]): u.Traverser =
+    l.reduceRight(_ compose _)(u)
 }
