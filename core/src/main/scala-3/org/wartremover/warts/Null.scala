18ef8cdc23d8855bbe351c68d30eefec0b464157
xuwei-k <6b656e6a69@gmail.com>
2022-03-17 21:45:20 +0900

    Null

--- core/src/main/scala-3/org/wartremover/warts/Null.scala
+++ core/src/main/scala-3/org/wartremover/warts/Null.scala
@@ -10,8 +10,18 @@ object Null extends WartTraverser {
           case t if hasWartAnnotation(t) =>
           case t if t.isExpr =>
             t.asExpr match {
-              case '{ $x } =>
-                x
+              case '{ null == ($x: Any)} =>
+              case '{ null != ($x: Any)} =>
+              case '{ ($x: Any) == null} =>
+              case '{ ($x: Any) != null} =>
+              case '{ null eq ($x: AnyRef)} =>
+              case '{ null ne ($x: AnyRef)} =>
+              case '{ ($x: AnyRef) eq null} =>
+              case '{ ($x: AnyRef) ne null} =>
+              case '{ null } =>
+                error(u)(tree.pos, "null is disabled")
+              case '{ ($x: Option[t]).orNull } =>
+                error(u)(tree.pos, "Option#orNull is disabled")
               case _ =>
                 super.traverseTree(tree)(owner)
             }
