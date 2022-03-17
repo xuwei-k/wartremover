29d802edb312be292ce93b30921834967e5ff29d
xuwei-k <6b656e6a69@gmail.com>
2022-03-17 21:38:41 +0900

    use Expr in ArrayEquals

--- core/src/main/scala-3/org/wartremover/warts/ArrayEquals.scala
+++ core/src/main/scala-3/org/wartremover/warts/ArrayEquals.scala
@@ -10,10 +10,16 @@ object ArrayEquals extends WartTraverser {
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
           case t if hasWartAnnotation(t) =>
-          case Apply(Select(receiver, "=="), Literal(NullConstant()) :: Nil) =>
-          case Apply(Select(receiver, "=="), _) if
-            (receiver.tpe.typeSymbol == TypeRepr.of[Array].typeSymbol) || (receiver.tpe <:< TypeRepr.of[Iterator[Any]]) =>
-            error(u)(tree.pos, "== is disabled, use sameElements instead")
+          case t if t.isExpr =>
+            t.asExpr match {
+              case '{ ($x1: Array[t]) == null} =>
+              case '{ ($x1: Array[t]) == ($x2: Any)} =>
+                error(u)(tree.pos, "== is disabled, use sameElements instead")
+              case '{ ($x1: Iterator[t]) == ($x2: Any)} =>
+                error(u)(tree.pos, "== is disabled, use sameElements instead")
+              case _ =>
+                super.traverseTree(tree)(owner)
+            }
           case _ =>
             super.traverseTree(tree)(owner)
         }
