bc23a649a81fada1beab67bbb175363bb1288656
xuwei-k <6b656e6a69@gmail.com>
2022-03-18 23:59:09 +0900

    add ForbidType

--- core/src/main/scala-3/org/wartremover/warts/Enumeration.scala
+++ core/src/main/scala-3/org/wartremover/warts/Enumeration.scala
@@ -1,19 +1,7 @@
 package org.wartremover
 package warts
 
-object Enumeration extends WartTraverser {
-  def apply(u: WartUniverse): u.Traverser = {
-    new u.Traverser {
-      import q.reflect.*
-      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
-        tree match {
-          case t if hasWartAnnotation(t) =>
-          case a: TypeTree if a.tpe =:= TypeRepr.of[scala.Enumeration] =>
-            error(u)(tree.pos, "Enumeration is disabled - use case objects instead")
-          case _ =>
-            super.traverseTree(tree)(owner)
-        }
-      }
-    }
-  }
-}
+object Enumeration
+    extends ForbidType[scala.Enumeration](
+      "Enumeration is disabled - use case objects instead"
+    )
--- core/src/main/scala-3/org/wartremover/warts/ScalaApp.scala
+++ core/src/main/scala-3/org/wartremover/warts/ScalaApp.scala
@@ -3,20 +3,7 @@ package warts
 
 import org.wartremover.WartTraverser
 
-object ScalaApp extends WartTraverser {
-  private def message = "Don't use scala.App. https://docs.scala-lang.org/scala3/book/methods-main-methods.html"
-  def apply(u: WartUniverse): u.Traverser = {
-    new u.Traverser {
-      import q.reflect.*
-      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
-        tree match {
-          case t if hasWartAnnotation(t) =>
-          case a: TypeTree if a.tpe =:= TypeRepr.of[scala.App] =>
-            error(u)(tree.pos, message)
-          case _ =>
-            super.traverseTree(tree)(owner)
-        }
-      }
-    }
-  }
-}
+object ScalaApp
+    extends ForbidType[scala.App](
+      "Don't use scala.App. https://docs.scala-lang.org/scala3/book/methods-main-methods.html"
+    )
