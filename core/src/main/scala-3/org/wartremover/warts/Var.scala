f334bf3865ffd4de6802f9823be75b01396d73df
xuwei-k <6b656e6a69@gmail.com>
2022-03-18 22:34:36 +0900

    wip

--- core/src/main/scala-3/org/wartremover/warts/Var.scala
+++ core/src/main/scala-3/org/wartremover/warts/Var.scala
@@ -10,7 +10,7 @@ object Var extends WartTraverser {
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
           case t if hasWartAnnotation(t) =>
-          case t: ValDef if t.symbol.flags.is(Flags.Mutable) =>
+          case t: ValDef if t.symbol.flags.is(Flags.Mutable) && !t.symbol.flags.is(Flags.Synthetic) =>
             error(u)(t.pos, "var is disabled")
           case _ =>
             super.traverseTree(tree)(owner)
