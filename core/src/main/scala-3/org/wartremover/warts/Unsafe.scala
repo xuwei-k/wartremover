70e00c73c96394411b4cec7798d9e266674b7954
xuwei-k <6b656e6a69@gmail.com>
2022-03-21 23:50:53 +0900

    Unsafe

--- core/src/main/scala-3/org/wartremover/warts/Unsafe.scala
+++
@@ -1,8 +0,0 @@
-package org.wartremover
-package warts
-
-object Unsafe extends WartTraverser {
-
-  def apply(u: WartUniverse): u.Traverser =
-    WartTraverser.sumList(u)(SafeWarts.safeTraversers)
-}
