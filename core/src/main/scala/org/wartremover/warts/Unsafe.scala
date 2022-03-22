f902885160e3042ca385f2d3d9a6c8ded3d50b05
xuwei-k <6b656e6a69@gmail.com>
2022-03-22 13:25:46 +0900

    [skip ci]

--- core/src/main/scala/org/wartremover/warts/Unsafe.scala
+++ core/src/main/scala/org/wartremover/warts/Unsafe.scala
@@ -2,7 +2,25 @@ package org.wartremover
 package warts
 
 object Unsafe extends WartTraverser {
+  val safeTraversers: List[WartTraverser] = List(
+    Any,
+    AsInstanceOf,
+    DefaultArguments,
+    EitherProjectionPartial,
+    IsInstanceOf,
+    IterableOps,
+    NonUnitStatements,
+    Null,
+    OptionPartial,
+    Product,
+    Return,
+    Serializable,
+    StringPlusAny,
+    Throw,
+    TryPartial,
+    Var
+  )
 
   def apply(u: WartUniverse): u.Traverser =
-    WartTraverser.sumList(u)(SafeWarts.safeTraversers)
+    WartTraverser.sumList(u)(safeTraversers)
 }
