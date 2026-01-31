ce4c33e0798910a9cc7d58470cd643db39f4444d
xuwei-k <6b656e6a69@gmail.com>
2026-01-31 15:15:44 +0900

    Optimize ForeachEntry

--- core/src/main/scala-3/org/wartremover/warts/ForeachEntry.scala
+++ core/src/main/scala-3/org/wartremover/warts/ForeachEntry.scala
@@ -6,9 +6,45 @@ object ForeachEntry extends WartTraverser {
     new u.Traverser(this) {
       import q.reflect.*
       private val mapSymbol = Symbol.requiredClass("scala.collection.Map")
+
+      val cache = {
+        import scala.jdk.CollectionConverters.*
+        new java.util.IdentityHashMap[SourceFile, Boolean]().asScala
+      }
+
+      private def existsForDoTree(src: SourceFile): Boolean = cache.getOrElseUpdate(
+        src, {
+          import dotty.tools.dotc.core.Contexts.Context
+          import dotty.tools.dotc.parsing.Parsers.Parser
+          import dotty.tools.dotc.ast.untpd.ForDo
+          import dotty.tools.dotc.ast.untpd.GenFrom
+          import dotty.tools.dotc.ast.untpd.Tuple
+          import dotty.tools.dotc.ast.untpd
+          import scala.quoted.runtime.impl.QuotesImpl
+
+          given c: Context = q.asInstanceOf[QuotesImpl].ctx
+          val untpdTree = c.compilationUnit.untpdTree
+
+          untpd.existsSubTree(untpdTree) {
+            case f: ForDo =>
+              f.enums.exists {
+                case GenFrom(Tuple(_ :: _ :: Nil), _, _) =>
+                  true
+                case _ =>
+                  false
+              }
+            case _ =>
+              false
+          }
+        }
+      )
+
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
-          case _ if sourceCodeNotContains(tree, "for") =>
+          case _
+              if (sourceCodeNotContains(tree, "foreach") && !existsForDoTree(
+                tree.pos.sourceFile
+              )) || sourceCodeNotContains(tree, "for") =>
           case t if hasWartAnnotation(t) =>
           case Apply(
                 TypeApply(Select(x, "foreach"), _),
