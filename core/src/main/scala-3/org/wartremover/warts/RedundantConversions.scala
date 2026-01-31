08fd6231bc20780a100b39921165f504ca9d2a83
xuwei-k <6b656e6a69@gmail.com>
2026-01-31 09:57:37 +0900

    wip

--- core/src/main/scala-3/org/wartremover/warts/RedundantConversions.scala
+++ core/src/main/scala-3/org/wartremover/warts/RedundantConversions.scala
@@ -2,64 +2,135 @@ package org.wartremover
 package warts
 
 import scala.annotation.nowarn
+import scala.quoted.Quotes
+import scala.quoted.quotes
 
 object RedundantConversions extends WartTraverser {
-  private val methodNames: Seq[String] = Seq(
-    "toList",
-    "toSeq",
-    "toVector",
-    "toStream",
-    "toSet",
-    "toIndexedSeq",
-    "toString",
-    "toInt",
-    "toLong",
-    "toFloat",
-    "toDouble",
-    "toByte",
-    "toShort",
-    "toChar",
-  )
 
   override def apply(u: WartUniverse): u.Traverser = {
     new u.Traverser(this) {
       import q.reflect.*
+      private final case class Value(
+        method: String,
+        check: PartialFunction[Select, String]
+      )
+
+      private val values: List[Value] = List(
+        Value(
+          "toList",
+          {
+            case s if s.qualifier.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.List") =>
+              "redundant toList conversion"
+          }
+        ),
+        Value(
+          "toSeq",
+          {
+            case s if s.qualifier.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Seq") =>
+              "redundant toSeq conversion"
+          }
+        ),
+        Value(
+          "toVector",
+          {
+            case s if s.qualifier.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Vector") =>
+              "redundant toVector conversion"
+          }
+        ),
+        Value(
+          "toStream",
+          {
+            case s if s.qualifier.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Stream") =>
+              "redundant toStream conversion"
+          }
+        ),
+        Value(
+          "toSet",
+          {
+            case s if s.qualifier.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Set") =>
+              "redundant toSet conversion"
+          }
+        ),
+        Value(
+          "toIndexedSeq",
+          {
+            case s if s.qualifier.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.IndexedSeq") =>
+              "redundant toIndexedSeq conversion"
+          }
+        ),
+        Value(
+          "toString",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[String] =>
+              "redundant toString conversion"
+          }
+        ),
+        Value(
+          "toInt",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Int] =>
+              "redundant toInt conversion"
+          }
+        ),
+        Value(
+          "toLong",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Long] =>
+              "redundant toLong conversion"
+          }
+        ),
+        Value(
+          "toFloat",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Float] =>
+              "redundant toFloat conversion"
+          }
+        ),
+        Value(
+          "toDouble",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Double] =>
+              "redundant toDouble conversion"
+          }
+        ),
+        Value(
+          "toByte",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Byte] =>
+              "redundant toByte conversion"
+          }
+        ),
+        Value(
+          "toShort",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Short] =>
+              "redundant toShort conversion"
+          }
+        ),
+        Value(
+          "toChar",
+          {
+            case s if s.qualifier.tpe <:< TypeRepr.of[Char] =>
+              "redundant toChar conversion"
+          }
+        )
+      )
+
+      private val methodNames: Seq[String] = values.map(_.method)
+
       @nowarn("msg=LazyList")
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
           case _ if methodNames.forall(sourceCodeNotContains(tree, _)) =>
           case t if hasWartAnnotation(t) =>
-          case s @ Select(t, method) =>
-            method match {
-              case "toList" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.List") =>
-                error(selectNamePosition(s), "redundant toList conversion")
-              case "toSeq" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Seq") =>
-                error(selectNamePosition(s), "redundant toSeq conversion")
-              case "toVector" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Vector") =>
-                error(selectNamePosition(s), "redundant toVector conversion")
-              case "toStream" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Stream") =>
-                error(selectNamePosition(s), "redundant toStream conversion")
-              case "toSet" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Set") =>
-                error(selectNamePosition(s), "redundant toSet conversion")
-              case "toIndexedSeq" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.IndexedSeq") =>
-                error(selectNamePosition(s), "redundant toIndexedSeq conversion")
-              case "toString" if t.tpe <:< TypeRepr.of[String] =>
-                error(selectNamePosition(s), "redundant toString conversion")
-              case "toInt" if t.tpe <:< TypeRepr.of[Int] =>
-                error(selectNamePosition(s), "redundant toInt conversion")
-              case "toLong" if t.tpe <:< TypeRepr.of[Long] =>
-                error(selectNamePosition(s), "redundant toLong conversion")
-              case "toFloat" if t.tpe <:< TypeRepr.of[Float] =>
-                error(selectNamePosition(s), "redundant toFloat conversion")
-              case "toDouble" if t.tpe <:< TypeRepr.of[Double] =>
-                error(selectNamePosition(s), "redundant toDouble conversion")
-              case "toByte" if t.tpe <:< TypeRepr.of[Byte] =>
-                error(selectNamePosition(s), "redundant toByte conversion")
-              case "toShort" if t.tpe <:< TypeRepr.of[Short] =>
-                error(selectNamePosition(s), "redundant toShort conversion")
-              case "toChar" if t.tpe <:< TypeRepr.of[Char] =>
-                error(selectNamePosition(s), "redundant toChar conversion")
-              case _ =>
+          case s @ Select(_, method) =>
+            values.iterator
+              .filter(x => sourceCodeContains(tree, x.method))
+              .find(_.method == method)
+              .flatMap(_.check.lift.apply(s)) match {
+              case Some(err) =>
+                error(selectNamePosition(s), err)
+              case None =>
                 super.traverseTree(tree)(owner)
             }
           case _ =>
